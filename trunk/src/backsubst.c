c1214ac906400cee8ddf85076099a02f5ffe3560
Majki1337 <mrefe12345@gmail.com>
2020-12-19 20:02:32 +0100

    poprawne_dzialanie_backa

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -1,21 +1,25 @@
 #include "backsubst.h"
+#include <stdio.h>
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-	/**
-	 * Tutaj należy umieścić właściwą implemntację. TEST DZIALANIA
-	 */
+	if (b->r != mat->r || mat->r != mat->c)
+		return 2;
+	int p = mat->r;
+	int n = p - 1; // zamiana na początku może być niewyraźna, ale po rozpisaniu na kartce
+	double sum = 0;// z n zamiast p-1 wszystko ładniej wygląda, bo w mallocu nie mamy miejsca na \0,
+	for(int i = n; i >= 0; i--){ // ktore siedzi w ostatniej komorce, na ktore trzeba uwazac...
+		if(mat->data[i][i] == 0) // sprawdza czy nie ma zera na diagonali
+			return 1; 
 
-	/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-	int i;
-	for (i =0; i < x->r; i++) {
-		x->data[i][0] = b->data[i][0];
+		for(int j = i+1; j < n+1; j++){
+			sum += mat->data[i][j] * x->data[j][0];
+		}
+		x->data[i][0] = (b->data[i][0] - sum)/mat->data[i][i];
 	}
-
 	return 0;
 }
 
